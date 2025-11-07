60c8c1065511a24744439c0f380cebc1f33848bd
Giannisduke <admin@eboy.gr>
2025-11-07 13:03:15 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles.scss';
+
+import $ from 'jquery';
+import { gsap } from "gsap";
+import '@wanoo21/countdown-time';
+
+
+
+// Παράδειγμα animation με gsap
+gsap.to(".element", { duration: 1, x: 100 });
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/js/script.js',
+    libraryTarget: 'module',
+  },
+  experiments: {
+    outputModule: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets:"defaults", modules:false }]],
+          },
+        },
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      scriptLoading: 'module',
+      inject: 'body',
+    }),
+  ],
+  mode: 'development',
+  resolve: {
+    fullySpecified: false,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    open: true,   // Αυτοματη εκκίνηση browser
+    hot: true,    // Hot Module Replacement
+  },
+};
